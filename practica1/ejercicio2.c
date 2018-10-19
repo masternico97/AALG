f57da9199849d524438ff55ad61c825b1c755bed
estecolm <36816018+estecolm@users.noreply.github.com>
2018-10-19 12:07:10 +0200

    Update ejercicio2.c

--- practica1/ejercicio2.c
+++ practica1/ejercicio2.c
@@ -35,8 +35,8 @@ int main(int argc, char** argv)
   }
 
   printf("Practica numero 1, apartado 2\n");
-  printf("Realizada por: Vuestros nombres\n");
-  printf("Grupo: Vuestro grupo\n");
+  printf("Realizada por: Nicolas Serrano y Miguel Luque\n");
+  printf("Grupo: 1212\n");
 
   /* comprueba la linea de comandos */
   for(i = 1; i < argc; i++) {
