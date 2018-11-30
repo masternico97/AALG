74e1324850022258171e7b564031b2004613e067
masternico97 <36814913+masternico97@users.noreply.github.com>
2018-11-30 10:19:42 +0100

    Add files via upload

---
+++ practica 3/busqueda.c
@@ -0,0 +1,150 @@
+/**
+ *
+ * Descripcion: Implementacion funciones para busqueda
+ *
+ * Fichero: busqueda.c
+ * Autor: Carlos Aguirre
+ * Version: 1.0
+ * Fecha: 11-11-2016
+ *
+ */
+
+#include "busqueda.h"
+
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ *  Funciones de geracion de claves
+ *
+ *  Descripcion: Recibe el numero de claves que hay que generar
+ *               en el parametro n_claves. Las claves generadas
+ *               iran de 1 a max. Las claves se devuelven en
+ *               el parametro claves que se debe reservar externamente
+ *               a la funcion.
+ */
+
+/**
+ *  Funcion: generador_claves_uniforme
+ *               Esta fucnion genera todas las claves de 1 a max de forma
+ *               secuencial. Si n_claves==max entonces se generan cada clave
+ *               una unica vez.
+ */
+void generador_claves_uniforme(int *claves, int n_claves, int max)
+{
+  int i;
+
+  for(i = 0; i < n_claves; i++) claves[i] = 1 + (i % max);
+
+  return;
+}
+
+
+PDICC ini_diccionario (int tamanio, char orden)
+{
+  PDICC dic = NULL;
+
+  if(tamanio < 1 || (orden != NO_ORDENADO && orden != ORDENADO)) return NULL;
+
+  dic=(PDICC)malloc(sizeof(DICC));
+  if(!dic) return NULL;
+
+  dic->tamanio=tamanio;
+  dic->orden=orden;
+  dic->n_datos=0;
+  dic->tabla=(int*)malloc(sizeof(int)*tamanio);
+
+  return dic;
+}
+
+void libera_diccionario(PDICC pdicc)
+{
+	if(!pdicc) return;
+  free(pdicc->tabla);
+  free(pdicc);
+}
+
+int inserta_diccionario(PDICC pdicc, int clave)
+{
+  int i;
+  int OB = 0 ;
+
+  if(!pdicc || pdicc->n_datos == pdicc->tamanio) return ERR;
+
+  pdicc->tabla[pdicc->n_datos]=clave;
+  pdicc->n_datos++;
+
+  if(pdicc->orden=ORDENADO){
+    i=pdicc->n_datos-1;
+    while (i >= 0 && pdicc->tabla[i]>clave){
+      pdicc->tabla[i+1]=pdicc->tabla[i];
+      i--;
+      OB++;
+    }
+    pdicc->tabla[i+1]=clave;
+  }
+  return OB;
+}
+
+int insercion_masiva_diccionario (PDICC pdicc,int *claves, int n_claves)
+{
+  int i, comprobacion;
+  int OB = 0;
+
+	if(!pdicc || !claves || n_claves==0) return ERR;
+
+  for(i=0; i<n_claves; i++){
+    comprobacion=inserta_diccionario(pdicc, claves[i]);
+    if(comprobacion == ERR) return ERR;
+    OB+=comprobacion;
+  }
+}
+
+int busca_diccionario(PDICC pdicc, int clave, int *ppos, pfunc_busqueda metodo)
+{
+  int OB = 0;
+  if(!pdicc || !ppos || !metodo) return ERR;
+  metodo(pdicc->tabla,0,(pdicc->n_datos)-1,clave,ppos);
+  return OB;
+}
+
+
+/* Funciones de busqueda del TAD Diccionario */
+int bbin(int *tabla,int P,int U,int clave,int *ppos)
+{
+  int OB = 0;
+
+  if(!tabla || P>U || !ppos) return ERR;
+
+  while(P<=U){
+    *ppos = (U+P)/2;
+    OB++;
+
+    if(clave == tabla[*ppos]) return OB;
+
+    else if(clave < tabla[*ppos]){
+      U = (*ppos)-1;
+    }
+    else{
+      P = (*ppos)+1;
+    }
+  }
+  return ERR;
+}
+
+int blin(int *tabla,int P,int U,int clave,int *ppos)
+{
+  int OB = 1;
+
+  if(!tabla || P>U || !ppos) return ERR;
+
+  *ppos = 0;
+  while(clave != tabla[(*ppos)] || (*ppos)<=U){
+    OB++;
+    (*ppos)++;
+  }
+
+  if((*ppos)>U) return ERR;
+
+  return OB;
+}
---
+++ practica 3/ordenacion.c
@@ -0,0 +1,325 @@
+/**
+ *
+ * Descripcion: Implementacion de funciones de ordenacion 
+ *
+ * Fichero: ordenacion.c
+ * Autor: Miguel Luque
+ * Version: 1.1
+ * Fecha: 28-09-2018
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "permutaciones.h"
+#include "ordenacion.h"
+/****************************************************/
+/* Funcion: minimo    Fecha:28/09/2018              */
+/* Vuestro comentario: esta función busca el       */
+/*elemento más pequeño y devuelve la posición de */
+/*la tabla en la que se sitúa                      */
+/****************************************************/
+
+int minimo(int *tabla, int ip, int iu) {
+    int i;
+    int val = ip;
+
+    if ((!tabla) || (ip < 0) || (iu < ip)) {
+        return ERR;
+    }
+
+    for (i = ip; i < iu; i++) {
+        if (tabla[i + 1] < tabla[val]) {/*operación básica*/
+            val = i + 1;
+        }
+
+    }
+    return (val);
+}
+
+/****************************************************/
+/* Funcion: maximo    Fecha:28/09/2018              */
+/* Vuestro comentario: esta función busca el       */
+/*elemento más grande y devuelve la posición de   */
+/*la tabla en la que se sitúa                      */
+/****************************************************/
+
+int maximo(int *tabla, int ip, int iu) {
+    int i;
+    int val = ip;
+
+    if ((!tabla) || (ip < 0) || (iu < ip)) {
+        return ERR;
+    }
+
+    for (i = ip; i < iu; i++) {
+        if (tabla[i + 1] > tabla[val]) {
+            val = i + 1;
+        }
+
+    }
+    return (val);
+}
+/****************************************************/
+/* Funcion: SelectSort    Fecha:28/09/2018          */
+/* Vuestro comentario: esta función busca el       */
+/*elemento más pequeño y lo coloca en la primera  */
+/*posición, acortando la tabla en cada ejecución  */
+/*para colocar el elemento mas pequeño exclullendo */
+/*las casillas ya ordenadas de la tabla             */
+/****************************************************/
+
+int SelectSort(int *tabla, int ip, int iu) {
+    int i, min, sum = 0;
+    if ((!tabla) || (ip < 0) || (iu < ip)) {
+        return ERR;
+    }
+
+    for (i = ip; i < iu; i++) {
+        min = minimo(tabla, i, iu);
+        if (min == ERR) {
+            return ERR;
+        }
+        sum += iu - i;
+        swap(&tabla[i], &tabla[min]);
+    }
+    return sum;
+}
+
+/***************************************************/
+/* Funcion: SelectSortInv    Fecha:28/09/2018      */
+/* Vuestro comentario: esta función busca el      */
+/*elemento más grande y lo coloca en la primera   */
+/*posición, acortando la tabla en cada ejecución */
+/*para colocar el elemento mas grande exclullendo  */
+/*las casillas ya ordenadas de la tabla            */
+/***************************************************/
+
+int SelectSortInv(int* tabla, int ip, int iu) {
+    int i, max, sum = 0;
+    if ((!tabla) || (ip < 0) || (iu < ip)) {
+        return ERR;
+    }
+
+    for (i = ip; i < iu; i++) {
+        max = maximo(tabla, i, iu);
+        if (max == ERR) {
+            return ERR;
+        }
+        sum += iu - i;
+        swap(&tabla[i], &tabla[max]);
+    }
+    return sum;
+}
+
+
+/***************************************************/
+/* Funcion: merge    Fecha:26/10/2018              */
+/* Vuestro comentario: 			           */
+/***************************************************/
+
+int merge(int* tabla, int ip, int iu, int imedio) {
+    int i = ip;
+    int j = imedio + 1;
+    int k = 0;
+    int op = 0;
+    int* taux = NULL;
+    if ((!tabla) || (ip < 0) || (iu < ip) || (imedio > iu) || (imedio < ip)) {
+        return ERR;
+    }
+    if (ip == iu) {
+        return 0;
+    }
+    taux = (int*) malloc((iu - ip + 1) * sizeof (int));
+    if (!taux) {
+        return ERR;
+    }
+    while (i <= imedio && j <= iu) {
+        op++;
+        if (tabla[i] < tabla[j]) {
+            taux[k] = tabla[i];
+            i++;
+        } else {
+            taux[k] = tabla[j];
+            j++;
+        }
+        k++;
+    }
+    if (i > imedio) {
+        while (j <= iu) {
+            taux[k] = tabla[j];
+            j++;
+            k++;
+        }
+    } else if (j > iu) {
+        while (i <= imedio) {
+            taux[k] = tabla[i];
+            i++;
+            k++;
+        }
+    }
+    for (i = ip, k = 0; i <= iu; i++, k++) {
+        tabla[i] = taux[k];
+    }
+    free(taux);
+    return op;
+}
+
+
+/***************************************************/
+/* Funcion: MergeSort    Fecha:26/10/2018          */
+/* Vuestro comentario: 			           */
+/***************************************************/
+
+
+int MergeSort(int* tabla, int ip, int iu) {
+    int M;
+    if ((!tabla) || (ip < 0) || (iu < ip)) {
+        return ERR;
+    }
+    if (ip == iu) {
+        return 0;
+    } else {
+        M = (ip + iu) / 2;
+        return MergeSort(tabla, ip, M) + MergeSort(tabla, M + 1, iu) + merge(tabla, ip, iu, M);
+    }
+}
+
+
+/***************************************************/
+/* Funcion: medio    Fecha:26/10/2018   	   */
+/* Vuestro comentario:		                   */
+/***************************************************/
+
+
+int medio(int *tabla, int ip, int iu, int *pos) {
+
+    if (!tabla || ip < 0 || iu < ip || !pos) {
+        return ERR;
+    }
+
+    *pos = ip;
+    return OK;
+}
+
+
+/***************************************************/
+/* Funcion: partir    Fecha:26/10/2018   	   */
+/* Vuestro comentario: 			           */
+/***************************************************/
+
+
+int partir(int* tabla, int ip, int iu, int *pos) {
+
+    int OB, pivote, i;
+
+    if (!tabla || ip < 0 || iu < ip || !pos) {
+        return ERR;
+    }
+
+    OB = medio(tabla, ip, iu, pos);
+    if (OB == ERR) {
+        return ERR;
+    }
+
+    pivote = tabla[*pos];
+
+    swap(&tabla[ip], &tabla[*pos]);
+    *pos = ip;
+
+    for (i = ip + 1; i <= iu; i++) {
+        OB++;
+        if (tabla[i] < pivote) {
+            (*pos)++;
+            swap(&tabla[i], &tabla[*pos]);
+        }
+    }
+    swap(&tabla[ip], &tabla[*pos]);
+    return OB;
+}
+
+/***************************************************/
+/* Funcion: QuickSort    Fecha:26/10/2018          */
+/* Vuestro comentario: 			           */
+/***************************************************/
+
+int QuickSort(int* tabla, int ip, int iu) {
+    int OB, pos, comprobacion;
+
+    if (!tabla || ip < 0 || iu < ip) {
+        return ERR;
+    }
+
+    if (ip == iu) {
+        return 0;
+    }
+
+    OB = partir(tabla, ip, iu, &pos);
+    if (OB == ERR) {
+        return ERR;
+    }
+
+    if (ip < pos - 1) {
+        comprobacion = QuickSort(tabla, ip, pos - 1);
+        if (comprobacion == ERR) {
+            return ERR;
+        }
+        OB += comprobacion;
+    }
+
+    if (pos + 1 < iu) {
+        comprobacion = QuickSort(tabla, pos + 1, iu);
+        if (comprobacion == ERR) {
+            return ERR;
+        }
+        OB += comprobacion;
+    }
+    return OB;
+}
+
+/***************************************************/
+/* Funcion: QuickSort_src    Fecha:02/11/2018      */
+/* Vuestro comentario: 			           */
+/***************************************************/
+int QuickSort_src(int* tabla, int ip, int iu) {
+    int OB, pos, comprobacion;
+
+    if (!tabla || ip < 0 || iu < ip) {
+        return ERR;
+    }
+
+    if (ip == iu) {
+        return 0;
+    }
+
+    OB = partir(tabla, ip, iu, &pos);
+    if (OB == ERR) {
+        return ERR;
+    }
+
+    if (ip < pos - 1) {
+        comprobacion = QuickSort(tabla, ip, pos - 1);
+        if (comprobacion == ERR) {
+            return ERR;
+        }
+        OB += comprobacion;
+    }
+
+    while (pos + 1 < iu) {
+        ip = pos + 1;
+        if (!tabla || ip < 0 || iu < ip) {
+            return ERR;
+        }
+        if (ip == iu) {
+            return OB;
+        }
+        comprobacion = partir(tabla, ip, iu, &pos);
+        if (ip < pos - 1) {
+            comprobacion += QuickSort(tabla, ip, pos - 1);
+        }
+        if (comprobacion == ERR) {
+            return ERR;
+        }
+        OB += comprobacion;
+    }
+    return OB;
+}
